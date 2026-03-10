492c0a2916c6346b863d71192a36ccceecef3964
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2026-03-10 12:31:23 +0700

    allow all linode cdn urls

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,31 +1,29 @@
-import {defineManifest} from '@crxjs/vite-plugin';
+import { defineManifest } from "@crxjs/vite-plugin";
 
 export default defineManifest({
   manifest_version: 3,
-  name: 'Immersion Kit Mining Helper',
-  version: '0.1.0',
-  description: 'Send Immersion Kit example sentence media to the latest Anki note.',
-  permissions: ['storage'],
+  name: "Immersion Kit Mining Helper",
+  version: "0.1.0",
+  description:
+    "Send Immersion Kit example sentence media to the latest Anki note.",
+  permissions: ["storage"],
   host_permissions: [
-    'https://www.immersionkit.com/*',
-    'https://immersionkit.com/*',
-    'https://apiv2.immersionkit.com/*',
-    'https://us-southeast-1.linodeobjects.com/*',
-    'http://127.0.0.1/*'
+    "https://www.immersionkit.com/*",
+    "https://immersionkit.com/*",
+    "https://apiv2.immersionkit.com/*",
+    "https://*.linodeobjects.com/*",
+    "http://127.0.0.1/*",
   ],
   background: {
-    service_worker: 'src/background/index.ts',
-    type: 'module'
+    service_worker: "src/background/index.ts",
+    type: "module",
   },
   content_scripts: [
     {
-      matches: [
-        'https://www.immersionkit.com/*',
-        'https://immersionkit.com/*'
-      ],
-      js: ['src/content/index.ts'],
-      run_at: 'document_idle'
-    }
+      matches: ["https://www.immersionkit.com/*", "https://immersionkit.com/*"],
+      js: ["src/content/index.ts"],
+      run_at: "document_idle",
+    },
   ],
-  options_page: 'src/options/index.html'
+  options_page: "src/options/index.html",
 });
